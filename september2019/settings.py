236cca921fb2e30a78dc1dbbc03fea9fad9ce97c
Sunil Kumar <sunil.kumar@nescode.com>
2019-09-26 16:14:12 +0530

    Settings update

--- september2019/settings.py
+++ september2019/settings.py
@@ -73,24 +73,13 @@ WSGI_APPLICATION = 'september2019.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# For production enable this db settings
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'september2019',
-        'USER': 'apple',
-        'PASSWORD': 'Indian@1979',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
